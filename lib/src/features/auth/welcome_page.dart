3f02cf8c69f6510ad9bd4494ecbd9952583583a6
Manio Khan <maniokhan2002@gmail.com>
2024-10-14 18:19:53 +0500

    new changes

--- lib/src/features/auth/welcome_page.dart
+++ lib/src/features/auth/welcome_page.dart
@@ -119,83 +119,103 @@ class LoginSignUpPage extends StatelessWidget {
               'assets/welcome_image.jpg',
               fit: BoxFit.cover,
             ),
-            Container(
-              height: MediaQuery.of(context).size.height / 1.8,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/welcome_image.png'))),
-            ),
-            // SizedBox(height: MediaQuery.of(context).size.height / 15
-            // ,),
-            Spacer(),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Container(
-                height: 60,
-                width: MediaQuery.of(context).size.width / 1.5,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15), color: Colors.red),
-                child: Center(
-                    child: Text(
-                  'Login',
+          ),
+          const BackgroundFilter(),
+          Positioned(
+            top: 260,
+            left: 70,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png', // Replace with your logo asset path
+                  height: 100,
+                  width: 100,
+                ),
+                const SizedBox(height: 16),
+                // Tagline Text
+                const Text(
+                  'Stay Safe, Stay Alert',
                   style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16,
-                      color: Colors.white),
-                )),
-              ),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(builder: (context) => LoginPage()),
-            //     );
-            //   },
-            //   child: Text('Login'),
-            // ),
-            SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignUpPage()),
-                );
-              },
-              child: Container(
-                height: 60,
-                width: MediaQuery.of(context).size.width / 1.5,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    // border: Border.all(color: Colors.red),
-                    color: Colors.white),
-                child: Center(
-                    child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16,
-                      color: Colors.red),
-                )),
-              ),
-            ),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(builder: (context) => SignUpPage()),
-            //     );
-            //   },
-            //   child: Text('Sign Up'),
-            // ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BackgroundFilter extends StatelessWidget {
+  const BackgroundFilter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (rect) {
+        return LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.white,
+            Colors.white.withOpacity(0.5),
+            Colors.white.withOpacity(0.0),
           ],
+          stops: const [
+            0.0,
+            0.4,
+            0.6,
+          ],
+        ).createShader(rect);
+      },
+      blendMode: BlendMode.dstOut,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.red.shade200,
+              Colors.red.shade800,
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const Spacer(),
+              // Login and SignUp Buttons
+              CommonButton(
+                color: Colors.grey,
+                text: "LOG IN",
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+              ),
+              CommonButton(
+                color: Colors.red,
+                text: "SIGN UP",
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
